bb724275a13088276d5feedf813125b5c5199427
Marcin Hojarski <hojarski.m@gmail.com>
2022-12-16 11:08:04 +0100

    fix01-Poprawki

---
+++ components/Chart.tsx
@@ -0,0 +1,155 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SegmentedRoundDisplay from 'react-native-segmented-round-display';
+
+type ChartT = {
+  chartName: string;
+  value: number;
+};
+const Chart = ({ chartName, value }: ChartT) => {
+  const whrChartColor = '#FDA901';
+  const bmiChartColor = '#8FBD12';
+
+  const getColor = () => {
+    return chartName.toLowerCase() === 'bmi' ? bmiChartColor : whrChartColor;
+  };
+
+  const getSummaryText = () => {
+    if (chartName.toLocaleLowerCase() === 'bmi') {
+      if (value < 18.5) {
+        return 'low';
+      }
+      if (value < 25) {
+        return 'Normal';
+      }
+      if (value <= 30) {
+        return 'Risen';
+      }
+      if (value <= 40) {
+        return 'High';
+      } else {
+        return 'Very high';
+      }
+    } else {
+      if (value <= 0.5) {
+        return 'Low';
+      }
+      if (value > 0.5 && value <= 1.5) {
+        return 'Average';
+      } else return 'High';
+    }
+  };
+
+  const getSegments = () => {
+    return chartName.toLocaleLowerCase() === 'bmi' ? bmiSegments : whrSegments;
+  };
+
+  const bmiSegments = [
+    {
+      total: 18.5,
+      filled: value < 18.5 ? value : 0 || (value >= 18.5 && 18.5),
+    },
+    {
+      total: 7,
+      filled: value >= 18.5 ? value - 18.5 : 0 || (value >= 24.99 && 7),
+    },
+    {
+      total: 5,
+      filled: value > 24.99 ? value - 25 : 0 || (value >= 30 && 5),
+    },
+    {
+      total: 10,
+      filled: value > 30 ? value - 30 : 0 || (value >= 40 && 10),
+    },
+    {
+      total: 10,
+      filled: value > 40 ? value - 40 : 0,
+    },
+  ];
+  const whrSegments = [
+    {
+      total: 0.5,
+      filled: value <= 0.5 ? value : 0 || (value > 0.5 && 0.5),
+    },
+    {
+      total: 1,
+      filled: value > 0.5 ? value - 0.5 : 0,
+    },
+    {
+      total: 1.5,
+      filled: value > 1.5 ? value - 1 : 0,
+    },
+  ];
+  return (
+    <View>
+      <SegmentedRoundDisplay
+        radius={60}
+        totalArcSize={180}
+        arcSpacing={10}
+        filledArcColor={getColor()}
+        incompleteArcColor={getColor()}
+        emptyArcColor="#F2EFDC"
+        valueBoxColor="blue"
+        valueBoxWidth={10}
+        segments={getSegments()}
+      />
+      <View style={styles().container}>
+        <Text style={styles().points}>{value}</Text>
+        <Text style={styles().chartName}>{chartName.toUpperCase()}</Text>
+        <View style={styles(chartName).summary}>
+          <Text style={styles(chartName).summaryText}>{getSummaryText()}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Chart;
+
+const styles = (chartName?: string) =>
+  StyleSheet.create({
+    container: {
+      bottom: 100,
+    },
+    points: {
+      alignSelf: 'center',
+      position: 'absolute',
+      fontFamily: 'Quicksand',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: 32,
+      lineheight: 40,
+      color: '#293C32',
+      bottom: 20,
+    },
+    chartName: {
+      alignSelf: 'center',
+      justifyContent: 'flex-start',
+      position: 'absolute',
+      fontFamily: 'Red Hat Display',
+      fontStyle: 'normal',
+      fontWeight: '400',
+      fontSize: 16,
+      lineHeight: 21,
+      textAlign: 'center',
+      color: '#293C32',
+    },
+    summary: {
+      width: 86,
+      height: 28,
+      backgroundColor: chartName?.toLocaleLowerCase() === 'bmi' ? '#EEF3DF' : '#FFF0D2',
+      borderRadius: 16,
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignSelf: 'center',
+      top: 30,
+    },
+    summaryText: {
+      fontFamily: 'Red Hat Display',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: 14,
+      lineHeight: 19,
+      color: chartName?.toLocaleLowerCase() === 'bmi' ? '#8FBD12' : '#FDA901',
+    },
+  });
--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 import HealthDetails from './HealthDetails';
-import BmiChart from './BmiChart';
-import ChartPannel from './ChartPannel';
-import WhrChart from './WhrChart';
+import Chart from './Chart';
 
 const MainContent = () => {
+  const bmiProps = {
+    chartName: 'Bmi',
+    value: 22,
+  };
+  const whrProps = {
+    chartName: 'Vhr',
+    value: 1,
+  };
+
   return (
     <>
-      <View style={styles.container2}>
+      <View style={styles.container}>
         <View>
           <Image source={require('../assets/body.png')} />
         </View>
         <View style={styles.charts}>
-          <View>
-            <ChartPannel Check={<BmiChart />} description="BMI" points="23.2" summary="Normal" />
-          </View>
-          <View>
-            <ChartPannel Check={<WhrChart />} description="WHR" points="0.96" summary="Average" />
-          </View>
+          <Chart chartName={bmiProps.chartName} value={bmiProps.value} />
+          <Chart chartName={whrProps.chartName} value={whrProps.value} />
         </View>
       </View>
       <HealthDetails />
@@ -27,8 +30,7 @@ const MainContent = () => {
 };
 
 const styles = StyleSheet.create({
-  container2: {
-    // position: 'absolute',
+  container: {
     width: 123.07,
     height: 342,
     left: 38,
@@ -37,7 +39,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   charts: {
-    left: 38,
+    top: 20,
     justifyContent: 'space-around',
   },
 });
